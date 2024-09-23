3737581c671732cb3ef2d962982147bf21725637
RaneemZi <raneemmahmoud333@gmail.com>
2024-09-23 10:05:20 +0300

    .

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->int('age');
+            $table->integer('age');
             $table->rememberToken();
             $table->timestamps();
         });
